1117b19428ca0f08cfd6ea2823289b17f1c0eb5f
Shruti Sandeep Kalyankar <80039672+shrutikalyankar@users.noreply.github.com>
2026-02-13 23:19:54 -0700

    Assignment 5 complete (#1)
    * Go to assignment 5
    
    * Fix save config script
    
    Correctly handle the case where multiple linux directories exist
    
    * Initial empty commit
    
    * Add buildroot submodule (2024.02.13)
    
    * assignment-4-complete
    
    * Remove stale buildroot submodule reference
    
    * re installed buildroot
    
    * Acknowledgements added
    
    * assignment 5 complete
    
    ---------
    
    Co-authored-by: Dan Walkes <danwalkes@trellis-logic.com>

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This repository contains assignment starter code for buildroot based assignments for the course Advanced Embedded Software Design, ECEN 5713
 
 It also contains instructions related to modifying your buildroot project to use with supported hardware platforms.  See [this wiki page](https://github.com/cu-ecen-5013/buildroot-assignments-base/wiki/Supported-Hardware) for details.
+Acknowledgements: https://chatgpt.com/share/698976d4-d360-8006-9f7b-35e180f8b92b
+
+As always, the chat gpt link is me running into infinite loops trying to figure out a small issue I made somewhere.
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,36 +6,43 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'bef620a90a044d26dc40b3877411fb11d16dd151'
+AESD_ASSIGNMENTS_VERSION = 40f9894f7823f842950d0508f53ee29c7f613fa3
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-shrutikalyankar.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-shrutikalyankar.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+AESD_ASSIGNMENTS_SUBDIR = assignment-3-shrutikalyankar
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
-        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/server clean
+
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/server
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-        # Create target directories
-        $(INSTALL) -d 0755 $(TARGET_DIR)/usr/bin
-        $(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf
+	# Create target directories
+	$(INSTALL) -d 0755 $(TARGET_DIR)/usr/bin
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf
 
-        # Install config files
-        $(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	# Install config files
+	$(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 
-        # Install scripts
-        $(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
-        $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
+	# Install scripts
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
 
-        # Install writer binary (built in BUILD_CMDS)
-        $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
+	# Install writer binary
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
 
-        # Optional: if you have tester.sh in your A3 repo
-        # $(INSTALL) -m 0755 $(@D)/finder-app/tester.sh $(TARGET_DIR)/usr/bin/
+	# Install aesdsocket binary
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
+
+	# Install init script
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
-
 $(eval $(generic-package))
